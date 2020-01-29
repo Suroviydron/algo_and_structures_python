1768fc0f92e2ec9b4c88d7f1ee06a0b4c09cbd5e
SuroviyDron <ablezgov@inbox.ru>
2020-01-30 00:49:22 +0300

    Небольшой Fix

--- Homework/hw07.3.py
+++ Homework/hw07.3.py
@@ -6,25 +6,26 @@
 # то используйте метод сортировки, который не рассматривался на уроках
 
 import timeit
+import copy
 from random import randint
 from statistics import median
 
 
 def find_median(orig_list):
 
-    for i in range(len(orig_list)):
+    arr_copy = copy.deepcopy(orig_list)
+    for i in range(len(arr_copy)):
         idx_min = i
         sort_check = False
-        for j in range(i + 1, len(orig_list)):
-            if orig_list[j] < orig_list[idx_min]:
+        for j in range(i + 1, len(arr_copy)):
+            if arr_copy[j] < arr_copy[idx_min]:
                 idx_min = j
                 sort_check = True
         if sort_check:
-            tmp = orig_list[idx_min]
-            orig_list[idx_min] = orig_list[i]
-            orig_list[i] = tmp
-
-    return orig_list
+            arr_copy[i], arr_copy[idx_min] = arr_copy[idx_min], arr_copy[i]
+    median_number = arr_copy[m]
+    del arr_copy
+    return median_number
 
 
 m = int(input('Введите значение числа m: '))
@@ -32,9 +33,9 @@ orig_list = [randint(-100, 100) for _ in range(2 * m + 1)]
 print(f'Изначальный массив: {orig_list}')
 print(f'Тестовая проверка через модуль Statistics: {median(orig_list)}')
 
-find_median(orig_list)
-print(f'Медиана отсортированного массива: {orig_list} равна {orig_list[m]}')
-# замеры m = 1000
+print(f'Медианное значение через сортировку: {find_median(orig_list)}')
+
+# замеры m = 500
 print(timeit.timeit("find_median(orig_list)", setup="from __main__ import find_median, orig_list", number=1000))
-# 3.753571 мс
+# 43.514376600000006 мс
 
